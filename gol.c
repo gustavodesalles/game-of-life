7e83b8f35f99e58abb4c6acff3f763e8d189097f
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2023-05-02 18:09:47 -0300

    altera play-parallel para usar ponteiro

--- gol.c
+++ gol.c
@@ -119,46 +119,46 @@ stats_t play(cell_t **board, cell_t **newboard, int size)
 
 void* play_parallel(void* arg)
 {
-    aux dados = *((aux*) arg);
+    aux *dados = (aux*) arg;
     // int begin = dados.i_begin * dados.size + dados.j_begin;
     // int end = dados.i_end * dados.size + dados.j_end;
     int x, a;
 
     stats_t stats = {0, 0, 0, 0};
-    dados.stats = &stats;
+    dados->stats = &stats;
 
     /* for each cell, apply the rules of Life */
     // for (i = 0; i < dados.size; i++)
     // {
-        for (x = dados.begin; x < dados.end; x++)
+        for (x = dados->begin; x < dados->end; x++)
         {
-            int i = x / dados.size;
-            int j = x % dados.size;
-            a = adjacent_to(dados.board, dados.size, i, j);
+            int i = x / dados->size;
+            int j = x % dados->size;
+            a = adjacent_to(dados->board, dados->size, i, j);
 
             /* if cell is alive */
-            if(dados.board[i][j]) 
+            if(dados->board[i][j]) 
             {
                 /* death: loneliness */
                 if(a < 2) {
-                    dados.newboard[i][j] = 0;
-                    dados.stats->loneliness++; // VERIFICAR ISSO
+                    dados->newboard[i][j] = 0;
+                    dados->stats->loneliness++; // VERIFICAR ISSO
                 }
                 else
                 {
                     /* survival */
                     if(a == 2 || a == 3)
                     {
-                        dados.newboard[i][j] = dados.board[i][j];
-                        dados.stats->survivals++;
+                        dados->newboard[i][j] = dados->board[i][j];
+                        dados->stats->survivals++;
                     }
                     else
                     {
                         /* death: overcrowding */
                         if(a > 3)
                         {
-                            dados.newboard[i][j] = 0;
-                            dados.stats->overcrowding++;
+                            dados->newboard[i][j] = 0;
+                            dados->stats->overcrowding++;
                         }
                     }
                 }
@@ -168,11 +168,11 @@ void* play_parallel(void* arg)
             {
                 if(a == 3) /* new born */
                 {
-                    dados.newboard[i][j] = 1;
-                    dados.stats->borns++;
+                    dados->newboard[i][j] = 1;
+                    dados->stats->borns++;
                 }
                 else /* stay unchanged */
-                    dados.newboard[i][j] = dados.board[i][j];
+                    dados->newboard[i][j] = dados->board[i][j];
             }
         }
     // }
