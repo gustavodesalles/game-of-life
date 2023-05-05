0c6a9cfba11825cd7354a3b7feacc7ab59f0c46e
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2023-05-05 15:41:57 -0300

    comentei "play" serial para não confundir

--- gol.c
+++ gol.c
@@ -60,62 +60,62 @@ int adjacent_to(cell_t **board, int size, int i, int j)
     return count;
 }
 
-stats_t play(cell_t **board, cell_t **newboard, int size)
-{
-    int i, j, a;
+// stats_t play(cell_t **board, cell_t **newboard, int size)
+// {
+//     int i, j, a;
 
-    stats_t stats = {0, 0, 0, 0};
+//     stats_t stats = {0, 0, 0, 0};
 
-    /* for each cell, apply the rules of Life */
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            a = adjacent_to(board, size, i, j);
+//     /* for each cell, apply the rules of Life */
+//     for (i = 0; i < size; i++)
+//     {
+//         for (j = 0; j < size; j++)
+//         {
+//             a = adjacent_to(board, size, i, j);
 
-            /* if cell is alive */
-            if(board[i][j]) 
-            {
-                /* death: loneliness */
-                if(a < 2) {
-                    newboard[i][j] = 0;
-                    stats.loneliness++;
-                }
-                else
-                {
-                    /* survival */
-                    if(a == 2 || a == 3)
-                    {
-                        newboard[i][j] = board[i][j];
-                        stats.survivals++;
-                    }
-                    else
-                    {
-                        /* death: overcrowding */
-                        if(a > 3)
-                        {
-                            newboard[i][j] = 0;
-                            stats.overcrowding++;
-                        }
-                    }
-                }
+//             /* if cell is alive */
+//             if(board[i][j]) 
+//             {
+//                 /* death: loneliness */
+//                 if(a < 2) {
+//                     newboard[i][j] = 0;
+//                     stats.loneliness++;
+//                 }
+//                 else
+//                 {
+//                     /* survival */
+//                     if(a == 2 || a == 3)
+//                     {
+//                         newboard[i][j] = board[i][j];
+//                         stats.survivals++;
+//                     }
+//                     else
+//                     {
+//                         /* death: overcrowding */
+//                         if(a > 3)
+//                         {
+//                             newboard[i][j] = 0;
+//                             stats.overcrowding++;
+//                         }
+//                     }
+//                 }
                 
-            }
-            else /* if cell is dead */
-            {
-                if(a == 3) /* new born */
-                {
-                    newboard[i][j] = 1;
-                    stats.borns++;
-                }
-                else /* stay unchanged */
-                    newboard[i][j] = board[i][j];
-            }
-        }
-    }
+//             }
+//             else /* if cell is dead */
+//             {
+//                 if(a == 3) /* new born */
+//                 {
+//                     newboard[i][j] = 1;
+//                     stats.borns++;
+//                 }
+//                 else /* stay unchanged */
+//                     newboard[i][j] = board[i][j];
+//             }
+//         }
+//     }
 
-    return stats;
-}
+//     return stats;
+// }
 
 void* play_parallel(void* arg)
 {
